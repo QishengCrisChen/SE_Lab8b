d3338bd9461a856c82607d64d737052dc2d4cc1c
Qisheng Chen <qchen@my.okcu.edu>
2019-03-27 10:24:59 -0500

    this is my second version#

--- Sum.c
+++ Sum.c
@@ -4,16 +4,19 @@
 #include<stdio.h>
 main(){
 
-  int i, Num, Sum = 0;
-
+  int i, Num, Sum = 0, counter;
+  float average;
+  
   printf("Please Enter the value of the Number : \n");
   scanf("%d", &Num);// enter your number
   printf("The value of Number is : %d \n", Num);
 
   for(i=1; i<=Num; i++){
     Sum +=i;
+    counter = i;
   }
     printf("The Sum of the number from 1 to %d is %d  \n", Num, Sum);
-  
+    printf("The average of the number is : %f \n", Sum/counter);
+    
   return 0;
 }// end of main
